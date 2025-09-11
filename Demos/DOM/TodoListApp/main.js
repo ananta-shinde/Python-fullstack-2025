1f5d0e603041babaabb645132ded96bb76b113b1
aShinde <ananta.shinde@honeywellcom>
2025-09-11 09:10:50 +0530

    11/09/2025

--- Demos/DOM/TodoListApp/main.js
+++ Demos/DOM/TodoListApp/main.js
@@ -4,22 +4,33 @@
 // task = "demo2"
 
 var taskList = []
+if(localStorage.getItem("taskList")){
+     taskList = JSON.parse(localStorage.getItem("taskList"))
+}
+
+displayTaskList()
+
 
 
 // display list
-function displayTaskList(){
+function displayTaskList(event){
+    var taskListContainer = document.getElementById("task-list")
     for(i=0;i<taskList.length;i++){
         task = taskList[i]
-        console.log(task.id+" "+task.description+" "+task.status)
+        var taskItem = document.createElement("li")
+        taskItem.innerText = task.description + " " + task.status
+        taskListContainer.appendChild(taskItem)
     }
 }
 // add new task
 function addNewTask(){
+   
     var task = {}
-    task.id = document.getElementById("taskId").value
+    task.id = taskList.length+1
     task.description =document.getElementById("description").value
     task.status = "pending"
     taskList.push(task)
+    localStorage.setItem("taskList",JSON.stringify(taskList))
 }
 //update task
 
