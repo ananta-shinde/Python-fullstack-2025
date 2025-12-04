dd4a90fdfff7c72a2d7d05b3b7dd737aaf3757da
aShinde <ananta.shinde@honeywellcom>
2025-12-04 10:19:27 +0530

    04/12/2025

--- ecom-frontend/src/pages/login.jsx
+++ ecom-frontend/src/pages/login.jsx
@@ -19,12 +19,15 @@ const LoginPage = () => {
         })
     }
     return ( 
-        <div className="container">
-            <div className="row">
-                <div className="col-6">
-                    <form method="post" onSubmit={handleSubmit}>
-                       { message != "" &&<p>{message}</p>}
-                        <input type="submit" value="submit"/>
+        <div className="container-fluid bg-light">
+            <div className="row ">
+                <div className="col-6 offset-3 d-flex " style={{height:"100vh"}}>
+                    <form className="d-block p-3 w-50 bg-white shadow mx-auto my-auto h-50" method="post" onSubmit={handleSubmit}>
+                        <h3 className="text-muted text-center">Login Here</h3>
+                       { message != "" &&<p className="alert alert-danger">{message}</p>}
+                       <input className="form-control my-3" type="email" placeholder="username"/>
+                       <input className="form-control my-3" type="password" placeholder="password"/>
+                        <input className="btn btn-dark w-100 my-3" type="submit" value="submit"/>
                     </form>
                 </div>
             </div>
