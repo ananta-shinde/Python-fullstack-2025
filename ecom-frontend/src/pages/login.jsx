eadfcd66965d2da34aba36155c4345e0b4815599
aShinde <ananta.shinde@honeywellcom>
2025-12-05 09:23:00 +0530

    05/12/2025

--- ecom-frontend/src/pages/login.jsx
+++ ecom-frontend/src/pages/login.jsx
@@ -2,20 +2,45 @@ import { useState } from "react"
 
 const LoginPage = () => {
     const [message,setMessage] = useState("")
+    // var username = undefined
+    // var password = undefined
+
+    var formdata = {}
+
+
+    // const handleUsernameChange = (event)=>{
+    //     username = event.target.value
+    // }
+    // const handlePasswordChange = (event)=>{
+    //     password = event.target.value
+    // }
+
+    const handleChange = (event)=>{
+        if(event.target.name == "username")
+        formdata.username = event.target.value
+        // username= event.target.value
+        if(event.target.name == "password")
+        formdata.password = event.target.value
+        // password = event.target.value
+    }
+
+
     const handleSubmit = (event)=>{
        
         event.preventDefault()
         fetch("https://dummyjson.com/auth/login",{
             method:"post",
             headers: { 'Content-Type': 'application/json' },
-            body:JSON.stringify({
-                username:"jshjdhjsdj",
-                password : "1212112"
-            })
+            body:JSON.stringify(formdata)
         })
         .then(res=>res.json())
         .then(res=>{
-            setMessage(res.message)
+            if(res.message){
+                setMessage(res.message)
+            }else{
+                setMessage("")
+            }
+            
         })
     }
     return ( 
@@ -25,8 +50,8 @@ const LoginPage = () => {
                     <form className="d-block p-3 w-50 bg-white shadow mx-auto my-auto h-50" method="post" onSubmit={handleSubmit}>
                         <h3 className="text-muted text-center">Login Here</h3>
                        { message != "" &&<p className="alert alert-danger">{message}</p>}
-                       <input className="form-control my-3" type="email" placeholder="username"/>
-                       <input className="form-control my-3" type="password" placeholder="password"/>
+                       <input className="form-control my-3" type="text" placeholder="username" name="username" required onChange={handleChange}/>
+                       <input className="form-control my-3" type="password" placeholder="password" name="password" required onChange={handleChange}/>
                         <input className="btn btn-dark w-100 my-3" type="submit" value="submit"/>
                     </form>
                 </div>
